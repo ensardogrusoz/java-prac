8a57dc2f3a2732eadef8f32b595a4e01be4e050f
Ensar Dogrusoz <42221230+ensardogrusoz@users.noreply.github.com>
2019-10-04 14:59:15 -0400

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -1,3 +1,8 @@
+/*
+will ask user to enter radius 1 and 2, then it will compare both radius, if they are the same radii it will print out area and 
+circumference. If they are different radii then it will print out area and circumference or both radii.
+*/
+
 package circle;
 
 import java.util.Scanner;
